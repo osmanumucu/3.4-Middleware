ebba2aa8a5b9821923790b32ae910d8ed0a47ca8
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-05-30 10:46:23 +0200

    feat: 204. Introduction to middlewares done

--- index1.js
+++ index1.js
@@ -1,10 +1,17 @@
-import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import express from "express";
+import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  console.groupCollapsed(req.body);
+})
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
